8eab2e5442669254c7fe4e53c352b8b732d4fc39
jananga99 <jananga1999@gmail.com>
2022-10-15 00:47:19 +0530

    cmd input add

--- main.py
+++ main.py
@@ -7,13 +7,25 @@ from functions.fileHandling import getConfigurationFromFile, writeFile, getPath
 from functions.pairedT import pairedT
 from functions.runTests import runTests
 
-"""
-start_file = "Sample_Start_Configuration.txt"
-goal_file = "Sample_Goal_Configuration.txt"
+while 1:
+    start_file = input("Start configuration filename    :   ")
+    try:
+        start_configuration = getConfigurationFromFile(start_file)
+        break
+    except FileNotFoundError:
+        print("Enter existing file name")
+
+while 1:
+    goal_file = input("Goal configuration filename     :   ")
+    try:
+        goal_configuration = getConfigurationFromFile(goal_file)
+        break
+    except FileNotFoundError:
+        print("Enter existing file name")
+
+
 output_file = "output.txt"
 
-start_configuration = getConfigurationFromFile(start_file)
-goal_configuration = getConfigurationFromFile(goal_file)
 size = len(start_configuration)
 heuMethod = "mis"
 
@@ -21,8 +33,9 @@ ans = n_search_set(start_configuration, goal_configuration, size, heuMethod)
 
 
 writeFile(output_file, getPath(ans))
-"""
 
-""""""
+
+"""
 runTests(100, 3, 4, "tests/starts/", "tests/goals/", "tests/outputs.txt")
 pairedT("tests/outputs.txt", "tests/analysis.txt")
+"""
